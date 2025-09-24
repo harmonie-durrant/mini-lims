29a72158c921dd7ff8a83178fe16376c42fc66de
harmonie-durrant <harmoniedurrant@gmail.com>
2025-09-24 21:13:46 +0200

    Add user get - first request using the database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models
+from .database import get_db
+
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"ok": True}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
