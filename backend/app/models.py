e6f7233c8e4252fccd39214c0367862a3de55eb2
harmonie-durrant <harmoniedurrant@gmail.com>
2025-09-25 16:20:31 +0200

    Add roles table

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,3 +10,44 @@ class User(Base):
     password_hash = Column(String, nullable=True)
     created_at = Column(DateTime, nullable=False, default=datetime.now)
     roles = Column(String, nullable=True)
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    position = Column(Integer, nullable=False)
+    description = Column(String, nullable=True)
+    icon = Column(String, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+    permissions = Column(String, nullable=True) # Comma-separated permissions
+
+"""
+    PERMISSIONS:
+
+    - manage_roles  | can create, read, update, delete roles with a higher position value
+
+    - create_users  | can create users with roles with higher position value
+    - read_users    | can read users with roles with higher position value
+    - update_users  | can update users with roles with higher position value
+    - delete_users  | can delete users with roles with higher position value
+
+    - create_samples  | can create samples
+    - read_samples    | can read samples
+    - update_samples  | can update samples
+    - delete_samples  | can delete samples
+
+    - create_tests  | can create tests
+    - read_tests    | can read tests
+    - update_tests  | can update tests
+    - delete_tests  | can delete tests
+
+    - create_results  | can create results
+    - read_results    | can read results
+    - update_results  | can update results
+    - delete_results  | can delete results
+    - validate_results | can validate results
+    - publish_results  | can publish results
+
+"""
