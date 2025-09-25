b33a2fc63159928636ee07be4f86e3ae170096ca
harmonie-durrant <harmoniedurrant@gmail.com>
2025-09-25 16:42:00 +0200

    Add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Mini LIMS</h1>
+      <p>
+        This is a minimal Laboratory Information Management System (LIMS) application. Made with ❤ by Harmonie Durrant.
+      </p>
+    </>
+  )
+}
+
+export default App
